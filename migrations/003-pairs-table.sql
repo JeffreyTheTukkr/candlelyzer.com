69853088fc053c00b620a93e5259c420500eb06a
Jeffrey <39796883+JeffreyTheTukkr@users.noreply.github.com>
2024-07-13 23:16:56 +0200

    add pairs table migration

---
+++ migrations/003-pairs-table.sql
@@ -0,0 +1,24 @@
+-- create enum type pair exchange
+CREATE TYPE pair_exchange AS ENUM ('binance', 'depreciated');
+
+-- create enum type pair status
+CREATE TYPE pair_status AS ENUM ('active', 'break', 'halt', 'end_of_day', 'delisted');
+
+-- create pairs table
+CREATE TABLE IF NOT EXISTS pairs (
+    id         uuid PRIMARY KEY       DEFAULT uuid_generate_v4(),
+    base       VARCHAR(8)    NOT NULL,
+    quote      VARCHAR(8)    NOT NULL,
+    exchange   pair_exchange NOT NULL,
+    status     pair_status   NOT NULL,
+    updated_at TIMESTAMPTZ   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMPTZ   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT unique_base_quote_exchange UNIQUE (base, quote, exchange)
+);
+
+-- add trigger function to automatically update the updated_at column
+CREATE TRIGGER update_pairs_updated_at_column
+    BEFORE UPDATE
+    ON pairs
+    FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
