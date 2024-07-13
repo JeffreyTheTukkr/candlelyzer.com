06a8e9a67185ede7a4069046e13f6d736a850d08
Jeffrey <39796883+JeffreyTheTukkr@users.noreply.github.com>
2024-07-13 23:07:43 +0200

    fmt

--- migrations/001-update-updated_at-column.sql
+++ migrations/001-update-updated_at-column.sql
@@ -1,6 +1,7 @@
 -- create function to automatically update the updated_at column
 CREATE OR REPLACE FUNCTION update_updated_at_column()
-    RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS
+$$
 BEGIN
     NEW.updated_at = CURRENT_TIMESTAMP;
     RETURN NEW;
