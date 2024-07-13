705fa70d89812c94b7448be9e6072e254f2bf530
Jeffrey <39796883+JeffreyTheTukkr@users.noreply.github.com>
2024-07-13 22:31:32 +0200

    add application struct to contain all global submodules

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"github.com/JeffreyTheTukkr/candlelyzer.com/databases"
+	"github.com/jackc/pgx/v5/pgxpool"
 	_ "github.com/joho/godotenv/autoload"
 )
 
+// Application struct to contain global submodules
+type Application struct {
+	DB *pgxpool.Pool
+}
+
+// NewApplication return a new application instance
+func NewApplication(db *pgxpool.Pool) *Application {
+	return &Application{
+		DB: db,
+	}
+}
+
 // main bootstrap application
 func main() {
-	fmt.Printf("candlelyzer.com")
+	// create psql database instance
+	db, dbErr := databases.CreatePsqlPool()
+	if dbErr != nil {
+		panic(dbErr)
+	}
+
+	// create new application
+	_ = NewApplication(db)
 }
