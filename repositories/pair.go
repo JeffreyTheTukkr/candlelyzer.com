67da5be16911a2b80408e6a85f6ea131b7847002
Jeffrey <39796883+JeffreyTheTukkr@users.noreply.github.com>
2024-07-15 14:08:47 +0200

    add basic crud operations

---
+++ repositories/pair.go
@@ -0,0 +1,49 @@
+package repositories
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/JeffreyTheTukkr/candlelyzer.com/models"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// Pair repository manager for the pair model
+type Pair struct {
+	db *pgxpool.Pool
+}
+
+// NewPair repository constructor for a new pair model repo
+func NewPair(db *pgxpool.Pool) *Pair {
+	return &Pair{
+		db: db,
+	}
+}
+
+// InsertOne insert a single pair base model
+func (p *Pair) InsertOne(m models.PairBase) error {
+	_, err := p.db.Exec(context.Background(), "INSERT INTO pairs (base, quote, exchange, status) VALUES ($1, $2, $3, $4);", m.Base, m.Quote, m.Exchange, m.Status)
+
+	return err
+}
+
+// FindById find a single pair model by id
+func (p *Pair) FindById(id uuid.UUID) (models.Pair, error) {
+	rows, err := p.db.Query(context.Background(), "SELECT * FROM pairs WHERE id = $1;", id)
+	if err != nil {
+		return models.Pair{}, err
+	}
+
+	return pgx.CollectOneRow(rows, pgx.RowToStructByName[models.Pair])
+}
+
+// UpdateStatusByIds update multiple pair models status by ids
+func (p *Pair) UpdateStatusByIds(ids []uuid.UUID, status models.PairStatus) error {
+	idList := strings.Trim(strings.Join(strings.Fields(fmt.Sprint(ids)), ", "), "[]")
+	_, err := p.db.Exec(context.Background(), "UPDATE pairs SET status = $1 WHERE id IN ($2);", status, idList)
+
+	return err
+}
