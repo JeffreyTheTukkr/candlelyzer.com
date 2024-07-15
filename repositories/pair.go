ff9abbb44bf0b941c67c976016db36246de92929
Jeffrey <39796883+JeffreyTheTukkr@users.noreply.github.com>
2024-07-15 17:57:52 +0200

    add upsert statement

--- repositories/pair.go
+++ repositories/pair.go
@@ -25,7 +25,8 @@ func NewPair(db *pgxpool.Pool) *Pair {
 
 // InsertOne insert a single pair base model
 func (p *Pair) InsertOne(m models.PairBase) error {
-	_, err := p.db.Exec(context.Background(), "INSERT INTO pairs (base, quote, exchange, status) VALUES ($1, $2, $3, $4);", m.Base, m.Quote, m.Exchange, m.Status)
+	_, err := p.db.Exec(context.Background(), "INSERT INTO pairs (base, quote, exchange, status) VALUES ($1, $2, $3, $4);",
+		m.Base, m.Quote, m.Exchange, m.Status)
 
 	return err
 }
@@ -38,6 +39,16 @@ func (p *Pair) FindById(id uuid.UUID) (models.Pair, error) {
 	}
 
 	return pgx.CollectOneRow(rows, pgx.RowToStructByName[models.Pair])
+}
+
+// UpsertOne update or insert a single pair model
+func (p *Pair) UpsertOne(m models.PairBase) error {
+	_, err := p.db.Exec(context.Background(), "INSERT INTO pairs (base, quote, exchange, status) VALUES ($1, $2, $3, $4) "+
+		"ON CONFLICT (base, quote, exchange) DO UPDATE "+
+		"SET status = EXCLUDED.status WHERE pairs.status IS DISTINCT FROM EXCLUDED.status;",
+		m.Base, m.Quote, m.Exchange, m.Status)
+
+	return err
 }
 
 // UpdateStatusByIds update multiple pair models status by ids
