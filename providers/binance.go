425405ebb93b49932de1085f272a4e260fcc25a9
Jeffrey <39796883+JeffreyTheTukkr@users.noreply.github.com>
2024-07-15 23:26:39 +0200

    refactor fetch candle data to return standardized candle model

--- providers/binance.go
+++ providers/binance.go
@@ -2,6 +2,7 @@ package providers
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/JeffreyTheTukkr/candlelyzer.com/models"
@@ -24,9 +25,10 @@ func NewBinanceRepo(apiKey string, secretKey string) *BinanceRepo {
 
 // ListAllPairs return a list of all binance pairs
 func (br *BinanceRepo) ListAllPairs() ([]models.PairBase, error) {
+	// retrieve data
 	exchange, err := br.client.NewExchangeInfoService().Do(context.Background())
 
-	// map binance data to standardized pair
+	// map data to standardized pair
 	var pairs []models.PairBase
 	for _, symbol := range exchange.Symbols {
 		return append(pairs, models.PairBase{
@@ -41,13 +43,37 @@ func (br *BinanceRepo) ListAllPairs() ([]models.PairBase, error) {
 }
 
 // FetchCandleData return candle data for a single pair
-func (br *BinanceRepo) FetchCandleData(pair string, since time.Time) ([]*binance.Kline, error) {
+func (br *BinanceRepo) FetchCandleData(pair string, since time.Time) ([]models.CandleBase, error) {
 	// override since time for dates before 2010
 	if since.UnixMilli() < time.Date(2010, 0, 0, 0, 0, 0, 0, time.UTC).UnixMilli() {
 		since = time.Date(2010, 0, 0, 0, 0, 0, 0, time.UTC)
 	}
 
-	return br.client.NewKlinesService().Symbol(pair).StartTime(since.UnixMilli()).Limit(1000).Interval("1m").Do(context.Background())
+	// retrieve data
+	data, err := br.client.NewKlinesService().Symbol(pair).StartTime(since.UnixMilli()).Limit(1000).Interval("1m").Do(context.Background())
+
+	// map data to standardized candle
+	var candles []models.CandleBase
+	for _, candle := range data {
+		openF, _ := strconv.ParseFloat(candle.Open, 64)
+		closeF, _ := strconv.ParseFloat(candle.Close, 64)
+		highF, _ := strconv.ParseFloat(candle.High, 64)
+		lowF, _ := strconv.ParseFloat(candle.Low, 64)
+		volumeF, _ := strconv.ParseFloat(candle.Volume, 64)
+
+		return append(candles, models.CandleBase{
+			OpenTime:  time.Unix(candle.OpenTime, 0),
+			CloseTime: time.Unix(candle.CloseTime, 0),
+			Open:      openF,
+			Close:     closeF,
+			High:      highF,
+			Low:       lowF,
+			Volume:    volumeF,
+			NoTrade:   uint64(candle.TradeNum),
+		}), nil
+	}
+
+	return candles, err
 }
 
 // matchBinancePairStatus helper to match the pair status from binance to standard
