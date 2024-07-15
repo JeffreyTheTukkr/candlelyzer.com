6500ed3533dc5abf801b894e2ded28c5497c4cd4
Jeffrey <39796883+JeffreyTheTukkr@users.noreply.github.com>
2024-07-15 22:35:40 +0200

    add basic pairs importer

---
+++ importers/pairs.go
@@ -0,0 +1,43 @@
+package importers
+
+import (
+	"log/slog"
+
+	"github.com/JeffreyTheTukkr/candlelyzer.com/providers"
+	"github.com/JeffreyTheTukkr/candlelyzer.com/repositories"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// PairsImporter required importer dependencies
+type PairsImporter struct {
+	db            *pgxpool.Pool
+	logger        *slog.Logger
+	binanceApiKey string
+	binanceSecret string
+}
+
+// NewPairsImporter constructor for new pairs importer
+func NewPairsImporter() *PairsImporter {
+	return &PairsImporter{}
+}
+
+// RunPairsImport run the pair importer
+func (pi *PairsImporter) RunPairsImport() {
+	// fetch binance pairs
+	binanceRepo := providers.NewBinanceRepo(pi.binanceApiKey, pi.binanceSecret)
+	pairs, err := binanceRepo.ListAllPairs()
+	if err != nil {
+		pi.logger.Error("failed to retrieve binance pairs", "error", err)
+	}
+
+	// create pairs repository
+	pairsRepo := repositories.NewPairRepo(pi.db)
+
+	// import pairs into database
+	for _, pair := range pairs {
+		err := pairsRepo.UpsertOne(pair)
+		if err != nil {
+			pi.logger.Error("failed to insert binance pair", "symbol", pair.Base+pair.Quote, "error", err)
+		}
+	}
+}
